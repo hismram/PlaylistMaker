e3092e2fc607f0e31618a0c2059e043d798f00dc
rr.khismatullin <hismram@gmail.com>
2024-06-23 13:06:47 +0500

    Домашняя работа по спринту 8

---
+++ app/src/main/java/com/example/playlismaker/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.playlismaker
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val search = findViewById<MaterialButton>(R.id.search)
+        val mediaLib = findViewById<MaterialButton>(R.id.media_lib)
+        val settings = findViewById<MaterialButton>(R.id.settings)
+
+        search.setOnClickListener {
+            startActivity(Intent(this, Search::class.java))
+        }
+
+        mediaLib.setOnClickListener {
+            startActivity(Intent(this, MediaLibrary::class.java))
+        }
+
+        settings.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+    }
+}
