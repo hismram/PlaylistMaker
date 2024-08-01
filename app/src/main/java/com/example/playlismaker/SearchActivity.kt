9492cb17fd51b9b1ba5a89e15d9fdb0a62fb0146
rr.khismatullin <hismram@gmail.com>
2024-08-02 00:24:33 +0500

    Добавил стили Добавил темы Вынес строки в strings.xml Развел ночной/дневной режимы цветовыми наборами Добавил обработку нажатий по пунктам на экране настроек Добавил верстку строки поиска и сброс введенного значения Добавил сохранение состояния ввода поиска

---
+++ app/src/main/java/com/example/playlismaker/SearchActivity.kt
@@ -0,0 +1,69 @@
+package com.example.playlismaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.ImageView
+
+class SearchActivity : AppCompatActivity() {
+    private var searchString: String = SEARCH_DEF
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+        // Поле ввода
+        val searchInput = findViewById<EditText>(R.id.search_input)
+        // Кнопка сброса введенного значения
+        val searchCross = findViewById<ImageView>(R.id.search_cross)
+
+        // Обработчики ввода
+        searchInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            // Обработка изменения введенного значения
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    searchCross.visibility = ImageView.GONE
+                } else {
+                    searchCross.visibility = ImageView.VISIBLE
+                }
+
+                // Сохраняем результат ввода в переменную
+                searchString = s.toString();
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        // Сброс поиска
+        searchCross.setOnClickListener {
+            searchInput.setText(SEARCH_DEF)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // Сохраняем состояние
+        outState.putString(SEARCH_STRING, searchString)
+    }
+
+    // Восстанавливаем состояние
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle
+    ) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val searchInput = findViewById<EditText>(R.id.search_input)
+
+        searchString = savedInstanceState.getString(SEARCH_STRING, SEARCH_DEF)
+        searchInput.setText(searchString)
+    }
+
+    companion object {
+        const val SEARCH_STRING = "SEARCH_STRING"
+        const val SEARCH_DEF = ""
+    }
+}
--- app/src/main/java/com/example/playlismaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlismaker/SettingsActivity.kt
@@ -1,13 +1,52 @@
 package com.example.playlismaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class SettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_settings)
 
+        val shareBtn = findViewById<TextView>(R.id.share)
+        val writeToSupportBtn = findViewById<TextView>(R.id.write_to_support)
+        val termsOfUseBtn = findViewById<TextView>(R.id.terms_of_use)
+
+        shareBtn.setOnClickListener { share() }
+        writeToSupportBtn.setOnClickListener { writeToSupport() }
+        termsOfUseBtn.setOnClickListener { termsOfUse() }
+    }
+
+    fun share() {
+        val intent = Intent(Intent.ACTION_SEND)
+
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            this.getString(R.string.course_link)
+        )
+        intent.setType("text/plain")
+
+
+        startActivity(intent)
+    }
+
+    fun writeToSupport() {
+        val intent = Intent(Intent.ACTION_SENDTO)
+
+        intent.setType("text/plain")
+        intent.data = Uri.parse("mailto:")
+        intent.putExtra(Intent.EXTRA_SUBJECT, this.getString(R.string.support_subject))
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(this.getString(R.string.support_mail)))
+        intent.putExtra(Intent.EXTRA_TEXT, this.getString(R.string.support_message))
+
+        startActivity(intent)
+    }
+
+    fun termsOfUse() {
+        val uri = Uri.parse(this.getString(R.string.terms_of_use_link))
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
     }
 }
