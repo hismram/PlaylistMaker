1ddf90af10f508806dbd91538ea1dc8649b2031a
rr.khismatullin <hismram@gmail.com>
2024-08-24 13:00:49 +0500

    Убрал дублирование кода

--- app/src/main/java/com/example/playlismaker/App.kt
+++ app/src/main/java/com/example/playlismaker/App.kt
@@ -13,35 +13,33 @@ class App: Application() {
     override fun onCreate() {
         sharedPref = getSharedPreferences(PREFERENCES_STORAGE_ID, MODE_PRIVATE)
         val mode = sharedPref.getInt(DARK_THEME, AppCompatDelegate.MODE_NIGHT_NO)
-        val uiModeManager = getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
 
         darkTheme = mode == AppCompatDelegate.MODE_NIGHT_YES
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            uiModeManager.setApplicationNightMode(UiModeManager.MODE_NIGHT_YES)
-        } else {
-            AppCompatDelegate.setDefaultNightMode(mode)
-        }
+        changeNightMode(mode)
 
         super.onCreate()
     }
 
     fun switchTheme(darkThemeEnabled: Boolean) {
         darkTheme = darkThemeEnabled
-        val uiModeManager = getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
         val nightMode =
             if (darkThemeEnabled) AppCompatDelegate.MODE_NIGHT_YES
             else AppCompatDelegate.MODE_NIGHT_NO
 
-        AppCompatDelegate.setDefaultNightMode(nightMode)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            uiModeManager.setApplicationNightMode(nightMode)
-        } else {
-            AppCompatDelegate.setDefaultNightMode(nightMode)
-        }
+        changeNightMode(nightMode)
 
         sharedPref.edit().putInt(DARK_THEME, nightMode).apply()
+    }
+
+    fun changeNightMode(mode: Int) {
+        val uiModeManager = getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            uiModeManager.setApplicationNightMode(mode)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(mode)
+        }
     }
 
     companion object {
