98e04f26e884b251a8913b8529610da16e07af80
rr.hismatullin <hismram@gmail.com>
2025-04-27 20:47:17 +0500

    ДЗ по спринту 17

--- app/src/main/java/com/example/playlismaker/di/DataModule.kt
+++ app/src/main/java/com/example/playlismaker/di/DataModule.kt
@@ -13,7 +13,7 @@ import com.example.playlismaker.sharing.domain.api.ExternalNavigator
 import com.google.gson.Gson
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidApplication
-import org.koin.dsl.module;
+import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.Executor
